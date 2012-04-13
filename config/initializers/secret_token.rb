2a192bd73fb0d8eb7336ae3f0ccd665778c751b8
Jim Challenger <jim.challenger1@gmail.com>
2012-04-13 00:06:17 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lolla2012::Application.config.secret_token = 'a0941c86b600a5ea5062a39f766189d1fbd09847d6f867d5d650826f56edd547317ff75dba4ac35a22b3b1e7cbd47cff832d77c818188aaeb3e82ce4e141dd56'
